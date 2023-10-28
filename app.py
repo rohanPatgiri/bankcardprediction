c8325e2b5085beb11a6a7862949739a22188dc2d
Rohan Patgiri <rohanpatgiri26@gmail.com>
2023-10-28 18:59:12 +0530

    added app.py file

---
+++ app.py
@@ -0,0 +1,44 @@
+import pickle
+from flask import Flask, request, app, url_for, render_template
+import numpy as np
+import pandas as pd
+
+
+app = Flask(__name__)
+
+# load the model
+model=pickle.load(open('BankCards.pickle','rb'))
+scalar = pickle.load(open('BankCardsPCA.pickle','rb'))
+
+
+@ app.route('/')
+def home():
+    return render_template('home.html')
+
+
+# input received through api
+@app.route('/predict_api', methods=['POST'])
+
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text= "The bank card type is {}".format(output))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
